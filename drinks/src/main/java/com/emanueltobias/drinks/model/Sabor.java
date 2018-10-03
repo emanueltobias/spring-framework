9e40c5e5b36473d818ee3d5b77ab5a7385e53f3e
EmanuelTobias <emanuel_tobias_@hotmail.com>
2018-10-03 12:35:02 -0300

    JPA, Hibernate e Flyway

---
+++ drinks/src/main/java/com/emanueltobias/drinks/model/Sabor.java
@@ -0,0 +1,17 @@
+package com.emanueltobias.drinks.model;
+
+public enum Sabor {
+
+	ADICICADA("Adocicada"), AMARGA("Amarga"), FRUTADA("Frutada"), SUAVE("Suave");
+
+	private String descricao;
+
+	Sabor(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+
+		return descricao;
+	}
+}
